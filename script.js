db5f0aad89b61d70d7c26d5c26eee86c93f8e85e
shsmink <79309340+shsmink@users.noreply.github.com>
2021-04-21 18:02:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-/*
 // No arguments
 
-const paintingWalls = function() {
-    console.log('The wall has been painted red');
-}
+// const paintingWalls = function() {
+//     console.log("The wall has been painted red");
+// }
 
-paintingWalls();
+// paintingWalls();
+
 
 // Single argument
 
-const paintingWalls = function(colors) {
-    console.log("The wall has been painted " + colors);
-}
+// const paintingWalls = function(color) {
+//     console.log("The wall has been painted " + color);
+// }
 
-paintingWalls("green");
-paintingWalls("blue");
+// paintingWalls("green");
+// paintingWalls("blue");
+// paintingWalls();
 
-// zonder de arguments gebeurt er niks
+// // 7. zonder een argument komt er nu undefined te staan op de plaats van de kleur.
 
-*/
 
 // Multiple arguments
 
-const paintingWalls = function (side, color) {
+// const paintingWalls = function (side, color) {
+//     console.log("The " + side + " wall has been painted " + color);
+// }
+
+// paintingWalls("north", "orange");
+// paintingWalls("south-east", "grey");
+
+// 6. de volgorde van de arguments bepaalt welk argument als eerste of als laatste wordt gebruikt.
+// 7. paintingWalls("grey", "south-east");  >>  The grey wall has been painted south-east
+// 8. de arguments worden alsnog op het juiste moment aangeroepen. Ze zijn afhankelijk van hun volgorde gekoppeld aan de arguments in de functie. In welke volgorde deze vervolgens gelogd worden, maakt niet uit. 
+
+const paintingWalls = function (color, side) {
     console.log("The " + side + " wall has been painted " + color);
 }
 
-paintingWalls("north", "orange");
-paintingWalls("south-east", "grey");
+paintingWalls("orange", "north");
+
+// The north wall has been painted orange
